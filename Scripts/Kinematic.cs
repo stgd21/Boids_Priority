8d50397e4910440babc4fbf51844b78480d4dd6a
stgd21 <45217325+stgd21@users.noreply.github.com>
2020-02-23 14:53:50 -0700

    Add files via upload

---
+++ Scripts/Kinematic.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//This purely holds data that needs to be referenced by other scripts
+public class Kinematic : MonoBehaviour
+{
+    [Header("Setting values here will only affect how other scripts view this gameObject")]
+    public Vector3 kPosition;
+    public float kOrientation;
+    public Vector3 kVelocity = Vector3.zero;
+    public float kRotation = 0f;
+    
+    public void GetData(SteeringOutput currentSteering)
+    {
+        if (currentSteering != null)
+        {
+            kVelocity += currentSteering.linear * Time.deltaTime;
+            kRotation += currentSteering.angular * Time.deltaTime;
+        }     
+    }
+
+    private void Update()
+    {
+        kPosition = transform.position;
+        kOrientation = transform.eulerAngles.y;
+    }
+}
